34858de27fb2801a52924f7b571837fc2c60871e
LolghElmo <35015411+LolghElmo@users.noreply.github.com>
2023-05-20 18:52:34 +0100

    Add files via upload

---
+++ GreatTutorialBot/GreatTutorialBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using GreatTutorialBot;
+using System.Threading;
+
+namespace GreatTutorailBot
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Bot bot = new Bot("5854978684:AAEhNYNiSXrAdbim-avmW_MtpW6CSzmKVC0");
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Enter Y or y To Turn On The Bot.");
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.KeyChar == 'y' || key.KeyChar == 'Y')
+                {
+                    bot.Start();
+                }
+            }
+        }
+    }
+}
